27008d5ec5e91036bedfa0a0cd8ee268dc54f49d
Kateryna Babakova <katerynababakova@unknownce9211904838.attlocal.net>
2025-10-30 09:45:19 -0500

    Debbaging

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,12 +1,12 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"Your chat ID is: {update.effective_chat.id}")
-app = Application.builder().token(BOT_TOKEN).build()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
