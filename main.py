fe6e3372fdb42f79030fa1423f49bbc902a0d300
Kateryna Babakova <katerynababakova@unknownce9211904838.attlocal.net>
2025-10-31 14:54:13 -0500

    debbug

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ WEBHOOK_URL = 'https://telegram-bot-1-wdbm.onrender.com/webhook'
 app = Flask(__name__)
 bot = Bot(token=TOKEN)
 tg_app = ApplicationBuilder().token(TOKEN).build()
+MESSAGES_FILE = "messages_by_date.json"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ Bot is working via webhook!")
@@ -29,6 +30,7 @@ async def send_daily_message():
             messages = json.load(f)
         if now in messages:
            text = messages[now]
+           await tg_app.bot_initialize()
            await tg_app.bot.send_message(chat_id=CHAT_ID, text=text)
            print(f"[{now}] Message send: {text}")
         else:
@@ -39,16 +41,19 @@ async def send_daily_message():
 
 async def scheduler_loop():
     while True:
-        await send_daily_message()
-        await asyncio.sleep(60)
+        now = datetime.now().strftime("%Y-%m-%dT%H")
+        if now.endswith("00") or now.endswith("30"):
+            await send_daily_message()
+            await asyncio.sleep(61)
+            print(datetime.now())
+        else:
+            await asyncio.sleep(20)
 
 @app.route("/webhook", methods=['POST'])
 async def webhook():
     try:
         data = request.get_json(force=True)
         print(f"Webhook received data: {data}")
-        update = Update.de_json(data, bot=tg_app.bot)
-        await tg_app.process_update(update)
         return "OK"
     except Exception as e:
         print(f"Webhook error: {e}")
@@ -56,12 +61,13 @@ async def webhook():
 @app.route("/")
 def home():
     return "Telegram bot is running"
+@app.before_request
+def activate_scheduler():
+    loop = asyncio.get_event_loop()
+    loop.create_task(scheduler_loop())
+
 
 
 if __name__ == "__main__":
-    tg_app.add_handler(CommandHandler("start", start))
-    async def main():
-        await tg_app.bot.set_webhook(url=WEBHOOK_URL)
-        asyncio.create_task(scheduler_loop())
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4545)))
-    asyncio.run(main())
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 4545)))
+
