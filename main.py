2da3159d46592cebc5f1d91894fdd999cfce2d4b
Kateryna Babakova <katerynababakova@unknownce9211904838.attlocal.net>
2025-10-31 10:25:18 -0500

    Fix error

--- main.py
+++ main.py
@@ -24,14 +24,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def send_daily_message():
     try:
+        now = datetime.now().strftime("%Y-%m-%dT%H")
         with open("messages_by_date.json", "r", encoding="utf-8") as f:
             messages = json.load(f)
-
-        now = datetime.now().strftime("%Y-%m-%dT%H")
-        text = messages.get(now)
-
-        if text:
-            await bot.send_message(chat_id=CHAT_ID, text=text)
+        if now in messages:
+           text = messages.get[now]
+           await bot.send_message(chat_id=CHAT_ID, text=text)
+           print(f"[{now}] Message send: {text}")
         else:
             print(f"[{now}] No message.")
     except Exception as e:
@@ -43,7 +42,7 @@ async def scheduler_loop():
         await send_daily_message()
         await asyncio.sleep(60)
 
-@app.post("/webhook")
+@app.route("/webhook", methods=['POST'])
 async def webhook():
     try:
         data = request.get_json(force=True)
@@ -61,12 +60,10 @@ def home():
 
 if __name__ == "__main__":
     tg_app.add_handler(CommandHandler("start", start))
+    loop = asyncio.get_event_loop()
+    loop.create_task(scheduler_loop())
 
     asyncio.get_event_loop().run_until_complete(
         bot.set_webhook(url=WEBHOOK_URL)
     )
-
-
-    loop = asyncio.get_event_loop()
-    loop.create_task(scheduler_loop())
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8443)))
